9160be7ea156ad74fc66d68eedf61ff636d54d4c
Bhrugu <BT0006@ajanmane.com>
2023-04-14 20:33:48 +0530

    bug: back click on slider

--- src/context/SlideMenuContext.tsx
+++ src/context/SlideMenuContext.tsx
@@ -30,7 +30,7 @@ export const SlideMenuContextProvider = ({
         }`}
       >
         <div className="flex items-center">
-          <div onClick={() => setOpenSlider(false)}>
+          <div onClick={() => setOpenSlider(true)}>
             <ArrowLeftIcon className="h-6 w-6" />
           </div>
           <span className="text-black font-semibold text-xl ml-4">
